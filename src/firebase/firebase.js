ae7ad062920e9f0e04f3b9dac7ec0a1d0c15d3a0
umair763 <muhammadumairkhan945@gmail.com>
2026-07-20 10:55:49 +0500

    env's

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,15 +5,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDWXg08LAVxRQ4bZ0LJcFy3ddsVJVS4lJA",
-  authDomain: "expense-track-playapp.firebaseapp.com",
-  projectId: "expense-track-playapp",
-  storageBucket: "expense-track-playapp.firebasestorage.app",
-  messagingSenderId: "389916813478",
-  appId: "1:389916813478:web:d1653506dc89d25f18ac67",
-  measurementId: "G-5D10VGB3TE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
